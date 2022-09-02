5bbf3a336fd79b5ea2716dd171b77a28bddeaae0
ayushjain27 <ayushjain201574@gmail.com>
2022-09-02 13:47:50 +0530

    Updated

--- src/Components/Selection/Selection.jsx
+++ src/Components/Selection/Selection.jsx
@@ -7,19 +7,19 @@ const Selection = () => {
         <div className="container">
             <h3 className=''>Selected Students</h3>
             <div className="row h-100 my-3" style={{height: '300px'}}>
-                <div className="col-md-3 position-relative">
+                <div className="col-md-3 my-3 position-relative">
                     <img className={`${styles.card} w-100 h-100`} src="./Images/download (1).jpeg" alt="" />
                     <p className={`${styles.text} position-absolute bottom-0 left-0 p-3 me-4 text-white fw-bold`}>Avinash Singh in Amazon with <span style={{color: 'yellow'}}>15000</span> stipend</p>
                 </div>
-                <div className="col-md-3 position-relative">
+                <div className="col-md-3 my-3 position-relative">
                     <img className={`${styles.card} w-100 h-100`} src="./Images/download.jpeg" alt="" />
                     <p className={`${styles.text} position-absolute bottom-0 left-0 p-3 me-4 text-white fw-bold`}>Sudha Bansal in Google with <span style={{color: 'yellow'}}>25000</span> stipend</p>
                 </div>
-                <div className="col-md-3 position-relative">
+                <div className="col-md-3 my-3 position-relative">
                     <img className={`${styles.card} w-100 h-100`} src="./Images/images.jpeg" alt="" />
                     <p className={`${styles.text} position-absolute bottom-0 left-0 p-3 me-4 text-white fw-bold`}>Anmol Sharma in Flipkart with <span style={{color: 'yellow'}}>12000</span> stipend</p>
                 </div>
-                <div className="col-md-3 position-relative">
+                <div className="col-md-3 my-3 position-relative">
                     <img className={`${styles.card} w-100 h-100`} src="./Images/download (2).jpeg" alt="" />
                     <p className={`${styles.text} position-absolute bottom-0 left-0 p-3 me-4 text-white fw-bold`}>Riya jain in Microsoft with <span style={{color: 'yellow'}}>10000</span> stipend</p>
                 </div>
